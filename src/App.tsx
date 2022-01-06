ef973db7eff83daddf4cba0b35a97de0330a8216
h662 <h662hong@gmail.com>
2022-01-06 22:33:26 +0900

    5. FrontendSetting1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React, { FC, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Main from "./routes/main";
 
 const App: FC = () => {
-  return <div>web3-boilerplate</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
