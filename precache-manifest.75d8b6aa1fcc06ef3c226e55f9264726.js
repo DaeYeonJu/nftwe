859dc4cfb55ae1a66da9fd3dba1d6a01f9ae5860
DaeYeonJu <dy1564@naver.com>
2022-06-07 01:14:08 +0900

    Updates

---
+++ precache-manifest.75d8b6aa1fcc06ef3c226e55f9264726.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bcd76bdfeca6b413cc1",
+    "url": "/manflex/static/js/main.5bcd76bd.chunk.js"
+  },
+  {
+    "revision": "f95b33de7e34430e3c7d",
+    "url": "/manflex/static/js/1.f95b33de.chunk.js"
+  },
+  {
+    "revision": "80793ec2ee46211f2ea9",
+    "url": "/manflex/static/js/2.80793ec2.chunk.js"
+  },
+  {
+    "revision": "4af94c156ce8fcec8d93",
+    "url": "/manflex/static/js/runtime~main.4af94c15.js"
+  },
+  {
+    "revision": "a17355064a472ba521da2daf5b119d76",
+    "url": "/manflex/index.html"
+  }
+];
