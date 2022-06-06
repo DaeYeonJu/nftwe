3a8ced7bca7abd509907f133ec9b1bdcd234c2d5
DaeYeonJu <dy1564@naver.com>
2022-06-07 02:01:32 +0900

    wqwqd

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,12 +16,12 @@ const Layout: FC = ({ children }) => {
         </Box>
         <Link to="/">
           <Button size="sm" colorScheme="green">
-            Main
+            Register
           </Button>
         </Link>
         <Link to="main">
           <Button size="sm" colorScheme="blue">
-            Register
+            Main
           </Button>
         </Link>
         <Link to="my-animal">
